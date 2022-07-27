24f726ea4a4cfdcc9a10b541e276d8bc2ed894e9
waCira <wachura11t@gmail.com>
2022-07-27 13:51:20 +0300

    Made some transformation

--- dbt/models/postgres_sample_data/postgres_sample_data_traffic_table.sql
+++ dbt/models/postgres_sample_data/postgres_sample_data_traffic_table.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 with source_traffic_table as (
     select * from {{ source('postgres_sample_data_reference', 'traffic_table') }}
 ), 
@@ -7,3 +9,7 @@ final as (
 )
 
 select * from final
+
+
+
+-- docker-compose run --rm server create_db, command to initialize postgres database to connect with redash
