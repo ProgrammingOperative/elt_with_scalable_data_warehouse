49f2805962d5a092d68552af09e36d498b4b401a
waCira <wachura11t@gmail.com>
2022-08-12 15:37:57 +0300

    joining two tables in warehouse worked!

--- dbt/models/pg_test/person_car.sql
+++ dbt/models/pg_test/person_car.sql
@@ -4,12 +4,16 @@ with source_person as(
     SELECT * FROM {{ source('masterdb', 'person') }}
 ),
 
-with source_car as (
+source_car as (
     SELECT * FROM {{ source('masterdb', 'car') }}
-)
+),
+
+joined as (
+    SELECT * FROM source_person LEFT JOIN source_car  USING (car_uid)
+),
 
 final as (
-    SELECT * FROM source_person
+    SELECT * FROM joined
 )
 
 select * FROM final
